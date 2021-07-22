dae9fd7b1339b327ae873a01a492d532d93c2c3d
niconicoj <joulin.nicolas@gmail.com>
2021-07-22 21:10:37 +0200

    add parser

--- src/AstPrinter.java
+++ src/AstPrinter.java
@@ -1,4 +1,4 @@
-public class AstPrinter implements Expr.Visitor<String> {
+class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -36,13 +36,5 @@ public class AstPrinter implements Expr.Visitor<String> {
         builder.append(")");
 
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
-                new Token(TokenType.STAR, "*", null, 1), new Expr.Grouping(new Expr.Literal(45.67)));
-
-        System.out.println(new AstPrinter().print(expression));
     }
 }
