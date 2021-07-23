bd8636185e0e69e3ac15ce27391000beefbc8d80
niconicoj <joulin.nicolas@gmail.com>
2021-07-23 09:33:20 +0200

    refactor ast generator

--- ast-generator/src/main.rs
+++ ast-generator/src/main.rs
@@ -1,5 +1,13 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
+use std::io::BufReader;
 use std::io::Write;
 use std::process;
 
@@ -9,16 +17,9 @@ use clap::Arg;
 type GenResult<T> = Result<T, Box<dyn Error>>;
 
 struct Config {
+    source: String,
     directory: String,
-    basename: String,
 }
-
-const TYPES: [&str; 4] = [
-    "Binary : Expr left, Token operator, Expr right",
-    "Grouping : Expr expression",
-    "Literal : Object value",
-    "Unary : Token operator, Expr right",
-];
 
 fn main() {
     let config = match get_args() {
@@ -28,10 +29,29 @@ fn main() {
             process::exit(1);
         }
     };
-    if let Err(e) = define_ast(config) {
-        eprint!("Error: {}", e);
-        process::exit(1);
+    let ast_config = match parse_source(config.source) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("Failed to parse ast config file : {}", e);
+            process::exit(1);
+        }
+    };
+    for (basename, types) in ast_config.into_iter() {
+        if let Err(e) = define_ast(&config.directory, &basename, types) {
+            eprint!("Error: {}", e);
+            process::exit(1);
+        }
     }
+}
+
+fn parse_source(source_path: String) -> GenResult<HashMap<String, Vec<String>>> {
+    let source_file = File::open(source_path)?;
+    let reader = BufReader::new(source_file);
+
+    let config = serde_json::from_reader(reader)?;
+
+    println!("{:?}", config);
+    Ok(config)
 }
 
 fn get_args() -> GenResult<Config> {
@@ -40,39 +60,40 @@ fn get_args() -> GenResult<Config> {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            Arg::with_name("basename")
-                .value_name("BASENAME")
-                .help("class Base name")
-                .default_value("Expr"),
+            Arg::with_name("source")
+                .help("source file")
+                .short("s")
+                .long("source")
+                .default_value("./ast.json"),
         )
         .arg(
             Arg::with_name("directory")
-                .help("Output directory")
+                .help("target directory")
                 .short("d")
                 .long("directory")
                 .default_value("./"),
         )
         .get_matches();
+    let source = matches.value_of("source").unwrap();
     let directory = matches.value_of("directory").unwrap();
-    let basename = matches.value_of("basename").unwrap();
     Ok(Config {
+        source: source.to_string(),
         directory: directory.to_string(),
-        basename: basename.to_string(),
     })
 }
 
-fn define_ast(config: Config) -> GenResult<()> {
-    let mut writer = File::create(format!("{}/{}.java", config.directory, config.basename))?;
+fn define_ast(directory: &String, basename: &String, types: Vec<String>) -> GenResult<()> {
+    let mut writer = File::create(format!("{}/{}.java", directory, basename))?;
     writer.write(b"import java.util.List;\n\n")?;
-    writer.write(format!("abstract class {} {{\n", config.basename).as_bytes())?;
-    define_visitor(&mut writer, config.basename.clone())?;
-    for t in TYPES.iter() {
+    writer.write(format!("abstract class {} {{\n", basename).as_bytes())?;
+    define_visitor(&mut writer, &basename, &types)?;
+    for t in types.iter() {
         let mut t = t.split(":");
         let classname = t.next().ok_or("no class name")?.trim();
         let fields = t.next().ok_or("no fields")?.trim();
         define_type(
             &mut writer,
-            config.basename.clone(),
+            &basename,
             classname.to_string(),
             fields.to_string(),
         )?;
@@ -85,7 +106,7 @@ fn define_ast(config: Config) -> GenResult<()> {
 
 fn define_type(
     writer: &mut File,
-    basename: String,
+    basename: &String,
     classname: String,
     fields: String,
 ) -> GenResult<()> {
@@ -113,9 +134,9 @@ fn define_type(
     Ok(())
 }
 
-fn define_visitor(writer: &mut File, basename: String) -> GenResult<()> {
+fn define_visitor(writer: &mut File, basename: &String, types: &Vec<String>) -> GenResult<()> {
     writer.write(b"    interface Visitor<R> {\n")?;
-    for t in TYPES.iter() {
+    for t in types.iter() {
         let type_name = t
             .split(":")
             .nth(0)
